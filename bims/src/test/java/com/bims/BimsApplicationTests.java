9c5e05acf9f0c221349976def2ef81c3c4a9b9b0
yueqiao <19301136@bjtu.edu.cn>
2022-08-05 18:01:41 +0800

    一个测试用的后端demo

---
+++ bims/src/test/java/com/bims/BimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
