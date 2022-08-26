da46891ee3ac398451e8328880b37e8bbfde305d
PeiQI1225 <101916168+PeiQI1225@users.noreply.github.com>
2022-08-26 20:24:40 +0800

    接口的完善

--- example_pkg/MySQL/mysqlconfig.py
+++ example_pkg/MySQL/mysqlconfig.py
@@ -1,19 +1,61 @@
 import pymysql
-from DBUtils.PooledDB import PooledDB
+import random
+import time
+
+# 创建数据库连接
+config = {
+    "host": 'localhost',  # 连接主机, 默认127.0.0.1
+    "user": 'root',  # 用户名
+    "passwd": 'root',  # 密码
+    "port": 3306,  # 端口，默认为3306
+    "db": 'bi_system',  # 数据库名称
+    "charset": 'utf8',  # 字符编码
+    "cursorclass": pymysql.cursors.DictCursor}
+
+conn = pymysql.connect(**config)
+# 生成游标对象 cursor
+cursor = conn.cursor()
 
 
-class MysqlServer():
-    def __init__(self):
-        self.maxusage = None
-        self.host = '127.0.0.1'
-        self.user = 'root'
-        self.passwd = 'root'
-        self.db = "bi_system"
-        self.port = 3306
-        self.charset = "utf8"
-        self.pool = PooledDB(pymysql, maxusage=self.maxusage, host=self.host, user=self.user,
-                             passwd=self.passwd, db=self.db, port=self.port, charset=self.charset)
+def insertdata(name: str, descr: str, dataSourceType: str, dbName: str, tableName: str, schema,
+               createUser: str):
+    app_id = random.randrange(1, 100)
+    insert_time = time.mktime(time.localtime())
+    sql = f'INSERT data_set VALUES(Null,{app_id},"{name}","{descr}","{dataSourceType}","{dbName}","{tableName}","{schema}","{createUser}",Null,{insert_time},Null,0);'
+    try:
+        cursor.execute(sql)
+        sql = f'select id from data_set where name = "{name}" and descr="{descr}" and data_source_type="{dataSourceType}" and db_name="{dbName}" and table_name="{tableName}" and create_user="{createUser}" and create_time = {insert_time};'
+        cursor.execute(sql)
+        dataSetId = cursor.fetchall()[0]
+        # 提交当前游标的全部操作
+        conn.commit()
+    except:
+        conn.rollback()
+        dataSetId = None
+    # 查看更新后的结果
+    return dataSetId
 
 
-conn = MysqlServer
-cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
+def getdataSetList(orderBy: str, order: str, page: int, pageSize: int, creatUser: str, keyword: str):
+    sql = f"SELECT * FROM data_set where create_user = '{creatUser}' and descr like '%{keyword}%' or name like '%{keyword}%' order by {orderBy},{order} LIMIT {(page - 1) * pageSize},{pageSize}; "
+    cursor.execute(sql)
+    return cursor.fetchall()
+
+
+def gettotalCount(creatUser: str, keyword: str):
+    sql = f"SELECT COUNT(*) FROM data_set where create_user = '{creatUser}' and descr like '%{keyword}%' or name like '%{keyword}%';"
+    cursor.execute(sql)
+    number = cursor.fetchall()
+    count = number[0]['COUNT(*)']
+    return count
+
+
+def deletedataSet(id:int):
+    try:
+        sql = f'DELETE FROM data_set WHERE id={id};'
+        cursor.execute(sql)
+        conn.commit()
+        return True
+    except:
+        conn.rollback()
+        return False
--- example_pkg/port_code/data_set_operate.py
+++ example_pkg/port_code/data_set_operate.py
@@ -1,11 +1,10 @@
 """数据集操作接口"""
-import datetime
+
 import json
 
-import random
-import time
-
 from fastapi import APIRouter
+
+from example_pkg.MySQL.mysqlconfig import insertdata, getdataSetList, gettotalCount, deletedataSet
 from example_pkg.Redis import RedisModel
 from example_pkg.click_house import databases
 from example_pkg.modules import Response
@@ -24,40 +23,15 @@ createUser:创建用户
 
 
 @dbs_operate.post('./dataset/create')
-async def create_dataset(name: str, descr: str, dataSourceType: str, dbName: str, tableName: str, schema: list,
+async def create_dataset(name: str, descr: str, dataSourceType: str, dbName: str, tableName: str, schema,
                          createUser: str):
-    # schema = [{'name': 'index'}, {"name": 'ts_code'}]
-    user_data = json.loads(RedisModel().get_data("user_data").get("result"))
-    cachedata = {}
-    # dataset_list = []
-    if dataSourceType == user_data.get("dataSourceType"):
-        try:
-            host = user_data.get('host')
-            port = user_data.get('port')
-            password = user_data.get('password')
-            user = user_data.get('user')
-            clients = databases(host=host, port=port, user=user, password=password)
-            # for i in schema:
-            #     schemas = i['name']
-            #     tabledatas = clients.Gettabledata(database=dbName, table=tableName, schema=schemas)
-            #     dataset_list.append(tabledatas)
-            cachedata['id'] = random.randrange(0, 100)
-            # cachedata['app_ip'] = random.randrange(0, 100)
-            cachedata['app_ip'] = 1
-            cachedata['name'] = name
-            cachedata['descr'] = descr
-            cachedata['data_source_type'] = dataSourceType
-            cachedata['db_name'] = dbName
-            # cachedata['schema'] = dataset_list
-            cachedata['schema'] = schema
-            cachedata['create_user'] = createUser
-            cachedata['update_user'] = None
-            cachedata['create_time'] = time.strftime("%Y-%m-%d %X")
-            cachedata['staus'] = 0
-            RedisModel().set_data(cachedata['app_ip'], json.dumps(cachedata))
-            return Response(data={'dataSetId': cachedata['app_ip']}, msg='success', code=0).return_response()
-        except:
-            return Response(data=None, msg='fail', code=110).return_response()
+    try:
+        dataSetId = insertdata(name=name, descr=descr, dataSourceType=dataSourceType, dbName=dbName,
+                               tableName=tableName
+                               , schema=schema, createUser=createUser)
+        return Response(data={'dataSetId': dataSetId}).return_response()
+    except:
+        return Response(data=None, msg='fail', code=110).return_response()
 
 
 '''数据集列表
@@ -72,7 +46,15 @@ keyword:数据集名称或数据集descr关键字搜索
 
 @dbs_operate.post('./list')
 async def dataset_list(orderBy: str, order: str, page: int, pageSize: int, creatUser: str, keyword: str):
-    pass
+    try:
+        data = {}
+        dataSetList = getdataSetList(orderBy=orderBy, order=order, page=page, pageSize=pageSize, creatUser=creatUser, keyword=keyword)
+        totalCount = gettotalCount(creatUser=creatUser, keyword=keyword)
+        data['dataSetList'] = dataSetList
+        data['totalCount'] = totalCount
+        return Response(data=data).return_response()
+    except:
+        return Response(data=None, msg='fail', code=110).return_response()
 
 
 """数据集删除"""
@@ -80,7 +62,9 @@ async def dataset_list(orderBy: str, order: str, page: int, pageSize: int, creat
 
 @dbs_operate.post('./delete')
 async def delete_dataset(id: int):
-    pass
+    if deletedataSet(id=id):
+        return Response(data=True).return_response()
+    return Response(data=False, msg='fail', code=110).return_response()
 
 
 """数据集信息"""
